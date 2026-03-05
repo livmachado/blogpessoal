f9cad973c7c2ac73062743c45902454c3232dc27
Livia Machado <liviamcampos98@gmail.com>
2026-03-05 00:07:19 -0300

    ✨ New Feature: Construção do Recurso Postagem

--- src/postagem/services/postagem.service.ts
+++ src/postagem/services/postagem.service.ts
@@ -1,7 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Postagem } from '../entities/postagem.entity';
-import { Repository } from 'typeorm';
+import { DeleteResult, ILike, Repository } from 'typeorm';
 
 @Injectable()
 export class PostagemService {
@@ -12,5 +12,44 @@ export class PostagemService {
 
   async findAll(): Promise<Postagem[]> {
     return await this.postagemRepository.find();
+  }
+
+  async findById(id: number): Promise<Postagem>{
+    const postagem = await this.postagemRepository.findOne({
+      where: {
+        id
+      }
+    })
+
+    if(!postagem)
+      throw new HttpException('Postagem não encontrada!', HttpStatus.NOT_FOUND)
+
+    return postagem;
+  }
+
+  async findAllByTitulo(titulo: string): Promise<Postagem[]>{
+    return this.postagemRepository.find({
+      where: {
+        titulo: ILike(`%${titulo}%`)
+      }
+    })
+  }
+  async create(postagem: Postagem): Promise<Postagem>{
+    return await this.postagemRepository.save(postagem);
+  }
+
+  async update(postagem: Postagem): Promise<Postagem>{
+    if(!postagem.id || postagem.id <=0){
+      throw new HttpException("O ID da postagem é inválido!",HttpStatus.BAD_REQUEST);
+    } 
+
+    await this.findById(postagem.id)
+    return await this.postagemRepository.save(postagem);
+  }
+
+  async delete(id: number) : Promise<DeleteResult>{
+    await this.findById(id);
+
+    return this.postagemRepository.delete(id);
   }
 }
